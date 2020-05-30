7efc869034f5d90ed7fe14d91fae2d9517a1eb21
bianca-promposal <johntedesco33@gmail.com>
2020-05-30 12:30:56 -0400

    Fixes image ref

--- src/Memory.js
+++ src/Memory.js
@@ -39,58 +39,58 @@ new Vue({
                 color: "white",
             },
             {
-                src: "projectphotos/photo2.jpg",
+                src: "ProjectPhotos/photo2.jpg",
                 quote: "Just how lucky I was to be with you",
                 color: "white",
             },
             {
-                src: "projectphotos/photo3.jpg",
+                src: "ProjectPhotos/photo3.jpg",
                 quote: "You have always been my number one supporter.",
                 color: "black",
             },
             {
-                src: "projectphotos/photo4.jpg",
+                src: "ProjectPhotos/photo4.jpg",
                 quote: "And together we've made memories that will last us forever.",
                 color: "white"
             }, 
             {
-                src: "projectphotos/photo5.jpg",
+                src: "ProjectPhotos/photo5.jpg",
                 quote: "We marked our spots in every place we've been.",
                 color: "black"
             },
             {
-                src: "projectphotos/photo6.jpg",
+                src: "ProjectPhotos/photo6.jpg",
                 quote: `You never failed to make me smile
                         And every adventure more memorable than the last.`,
                 color: "black",
             },
             {
-                src: "projectphotos/photo7.jpg",
+                src: "ProjectPhotos/photo7.jpg",
                 quote: "Some might say our relationship is quite explosive",
                 color: "white",
             },
             {
-                src: "projectphotos/photo8.jpg",
+                src: "ProjectPhotos/photo8.jpg",
                 quote: "But I think the only thing exploding is our laughter",
                 color: "white"
             },
             {
-                src: "projectphotos/photo9.jpg",
+                src: "ProjectPhotos/photo9.jpg",
                 quote: "Even though you always steal my sweaters...",
                 color: "white",
             },
             {
-                src: "projectphotos/photo10.jpg",
+                src: "ProjectPhotos/photo10.jpg",
                 quote: "You will always be the most beautiful girl I know",
                 color: "white"
             },
             {
-                src: "projectphotos/photo11.jpg",
+                src: "ProjectPhotos/photo11.jpg",
                 quote: "Even though the times have changed...",
                 color: "white"
             },
             {
-                src: "projectphotos/photo12.jpg",
+                src: "ProjectPhotos/photo12.jpg",
                 quote: "I will always pre-love you.",
                 color: "black",
             },
